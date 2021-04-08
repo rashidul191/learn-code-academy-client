7c0e16718338fff1194088e876a64ee74734e6a1
rashidul191 <rashidul.191cse.gub@gmail.com>
2021-04-08 12:54:41 +0600

    create components file , share file, home, navbar , footer etc

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Learn Code Academy run build</h1>
+        </div>
+    );
+};
+
+export default Home;
