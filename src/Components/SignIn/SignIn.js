fc593e105865b9d6dd66d407170da90bc4c35a34
rashidul191 <rashidul.191cse.gub@gmail.com>
2021-04-10 23:13:57 +0600

    sign in components doen

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import Footer from '../ShareFile/Footer/Footer';
+import Navbar from '../ShareFile/Navbar/Navbar';
 import './SignIn.css';
 
 const SignIn = () => {
     return (
         <section id="sign-in">
-            <h1>Sign In page comming soon.....</h1>
+            <Navbar></Navbar>
+            <div className="sign-in-img">
+                <div className="sign-in-bg">
+                    <div className="container py-4">
+                        <h3 className="display-4 text-light pb-3">Sign In</h3>
+                        <div>
+                            <div className="row">
+                                <div className="col-lg-6 col-md-10 sign-body-bg">
+                                    <form action="">
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name:</label>
+                                            <input type="text" className="form-control form-control-lg " name="name" id="name" placeholder="Your Name"  required/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="email">Email:</label>
+                                            <input type="email" className="form-control form-control-lg " name="email" id="email" placeholder="name@example.com" required/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label htmlFor="password">Password:</label>
+                                            <input type="password" className="form-control form-control-lg " name="password" id="password" placeholder="Your password" required/>
+                                        </div>
+                                        <div className="row mx-1">
+                                            <div class="form-group form-check">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                                <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                                            </div>
+                                            <div className="ml-auto">
+                                                <a href="">Create a New Account</a>
+                                            </div>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <input type="submit" value="Submit" className="btn btn-success btn-block btn-lg" />
+                                        </div>
+                                    </form>
+                                    <div>
+                                        <p className="lead text-center text-primary"> Or </p>
+                                    </div>
+                                    <div>
+                                        <button>Google With Sign In</button>
+                                    </div>
+                                    <br/>
+                                    <div>
+                                        <button>Facebook With Sing IN</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Footer></Footer>
         </section>
     );
 };
